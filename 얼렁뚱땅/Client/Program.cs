069df12fb88b7c11913c6f91f725795a56786fb5
toast-ceo <70907737+toast-ceo@users.noreply.github.com>
2022-10-25 14:34:38 +0900

    2022.10.25

---
+++ 얼렁뚱땅/Client/Program.cs
@@ -0,0 +1,125 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+
+namespace AClient 
+{
+    public class Client
+    {
+        private readonly static int BufferSize = 4096;
+
+        public static void Main()
+        {
+            try
+            {
+                new Client().Init();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("Press any key to exit the program.");
+            Console.ReadKey();
+        }
+
+
+        private Socket clientSocket;
+        public Socket ClientSocket
+        {
+            get => clientSocket;
+            set => clientSocket = value;
+        }
+        private readonly IPEndPoint EndPoint = new(IPAddress.Parse("127.0.0.1"), 5001);
+
+        public Client() // 1. 소켓 생성
+        {
+            ClientSocket = new(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp
+            );
+        }
+
+        void Init() // 
+        {
+            ClientSocket.Connect(EndPoint); // 2. 서버 연결
+            Console.WriteLine($"Server connected.");
+
+            SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+            args.Completed += new EventHandler<SocketAsyncEventArgs>(Received);
+            ClientSocket.ReceiveAsync(args);
+
+            Send(); // 3-1. 전송
+        }
+
+
+        void Received(object? sender, SocketAsyncEventArgs e)
+        {
+            try // 확장 가능성 있음
+            {
+                byte[] data = new byte[BufferSize];
+                Socket server = (Socket)sender!;
+                int n=server.Receive(data);
+
+                string str = Encoding.Unicode.GetString(data);
+                str = str.Replace("\0", "");
+                Console.WriteLine("수신:"+str); // Split 예쁘게 구분해서 상세하게 보여주게 할 수 있음
+
+
+                SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+                args.Completed += new EventHandler<SocketAsyncEventArgs>(Received);
+                ClientSocket.ReceiveAsync(args);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"Server disconnected.");
+                ClientSocket.Close();
+            }
+        }
+
+        void Send()
+        {
+            byte[] dataID;
+            Console.WriteLine("ID를 입력하세요");
+            string nameID = Console.ReadLine()!;
+            // ReadLine() ! => 느낌표 내 코드상 널이 아니야
+            byte[] massage = "ID:" + nameID + ":";
+            dataID = Encoding.Unicode.GetBytes(message);
+            clientSocket.Send(dataID);
+            //
+
+            Console.WriteLine("특정 사용자에게 보낼 때는 사용자ID:메시지 로 입력하시고\n" +
+                "브로드캐스트하려면 BR:메시지 를 입력하세요");
+            do // 3-1. 읽어서 전송 무한 반복
+            {
+                // 입력할 때 까지 기다리는 곳
+                byte[] data; 
+                string msg = Console.ReadLine()!;
+                string[] tokens = msg.Split(':');
+                string m;
+                if (tokens[0].Equals("BR"))
+                {
+                    // 브로드캐스트 메세지
+                    m = "BR:" + nameID + ":" + totoken[1]; // "BR" 메세지 서버로 감 서버쪽 receive 함수로 이동
+
+                    data = Encoding.Unicode.GetBytes(m);
+                    Console.WriteLine("[전체전송]{0}", tokens[1]);
+                }
+                else //  (tokens[0].Equals("TO"))
+                {
+                    //
+                    m = "TO:" + nameID + ":" + tokens[1] + ":" + tokens[2] + ":";
+                    data = Encoding.Unicode.GetBytes(m);
+                    Console.WriteLine("[{0}에게 전송]:{1}", tokens[0], tokens[1]);
+                 }
+
+                try { ClientSocket.Send(data); } catch { }
+
+
+            } while (true);
+        }
+    }
+}
---
+++ 얼렁뚱땅/Server/Program.cs
@@ -0,0 +1,202 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace AServer
+{
+    public class Server
+    {
+        private readonly static int BufferSize = 4096;
+
+        public static void Main()
+        {
+            try
+            {
+                new Server().Init(); // 서버 시작
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        // Dictionary 키값으로 간편하게 연결?
+        private Dictionary<string, Socket> connectedClients = new();
+
+        public Dictionary<string, Socket> ConnectedClients
+        {
+            get => connectedClients;
+            set => connectedClients = value;
+        }
+
+        private Socket ServerSocket;
+
+        private readonly IPEndPoint EndPoint = new(IPAddress.Parse("127.0.0.1"), 5001);
+
+        int clientNum; // 크리티컬 섹션 공유 자원(변수)... 접근성 적용 해야함
+        Server() // 1. 소켓 생성 @ 생성자
+        {
+            ServerSocket = new(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp
+            );
+            clientNum = 0;
+        }
+
+        void Init()
+        {
+            ServerSocket.Bind(EndPoint);    // 2. Bind
+            ServerSocket.Listen(100);       // 3. Listen
+            Console.WriteLine("Waiting connection request.");
+
+            Accept();                       // 4-1. Accept 호출
+
+        }
+ 
+
+        void Accept()
+        {
+            do
+            {
+                Socket client = ServerSocket.Accept(); // 4-2. Accept() 클라이언트와 연결
+
+                
+                Console.WriteLine($"Client accepted: {client.RemoteEndPoint}.");
+
+                SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+                args.Completed += new EventHandler<SocketAsyncEventArgs>(Received);
+                client.ReceiveAsync(args);
+
+            } while (true);     // 4-3. 무한 반복
+        }
+
+        void Disconnected(Socket client)    // 6. 소켓 종료 , 연결이 끊겼다! 하면 id를 찾아 삭제함
+        { 
+            Console.WriteLine($"Client disconnected: {client.RemoteEndPoint}."); 
+            foreach (KeyValuePair<string, Socket> clients in connectedClients)
+            {
+                if (clients.Value == client)
+                {
+                    ConnectedClients.Remove(clients.Key);
+                    clientNum--;
+                }
+            }
+            client.Disconnect(false);
+            client.Close();
+        }
+
+        void Received(object? sender, SocketAsyncEventArgs e)
+        {
+            Socket client = (Socket)sender!; // 누가 보냈는지 알 수 있게 됨
+            byte[] data = new byte[BufferSize];
+            try
+            {
+                int n = client.Receive(data); // 5-1. 데이터 받기
+                if (n > 0) // 0 보다 크면 정상적인 연결
+                {
+
+                    // 다른 메세지를 처리하는 함수가 들어있음 
+                    // 누가 보냈는지 받은 데이터 넘겨줌
+                    MessageProc(client, data);
+
+                    // 반복적으로 데이터를 받기 위해 다시 등록함 (대기하도록)
+                    SocketAsyncEventArgs argsR = new SocketAsyncEventArgs();
+                    argsR.Completed += new EventHandler<SocketAsyncEventArgs>(Received);
+                    client.ReceiveAsync(argsR);
+                }
+                else { throw new Exception(); } // 정상적이지 않을 때
+            } catch (Exception) { // 캐치문으로 들어감
+                Disconnected(client); // 6. 전송 오류 나면 종료
+            }
+        }
+
+        void MessageProc(Socket s, byte[] bytes)
+        {
+            string m = Encoding.Unicode.GetString(bytes);
+            // 메세지를 상세하게 만드는 곳
+            string[] tokens = m.Split(":"); // :으로 구분해놓은 곳을 더 상세하게
+            string fromID;
+            string toID;
+            string code = tokens[0]; // ID
+
+            if (code.Equals("ID"))
+            {
+                clientNum++; // 클라이언트 추가 생성시 필요
+                fromID = tokens[1].Trim(); // Trim?
+                Console.WriteLine("[접속{0}]ID:{1},{2}",
+                    clientNum, fromID, s.RemoteEndPoint);
+                // 클라이언트에 아이디 추가
+                connectedClients.Add(fromID, s);
+                s.Send(Encoding.Unicode.GetBytes("ID_REG_Success:")); // ID 등록 성공
+                Broadcast(s, m); // 소켓값 하나 추가
+            }
+            else if (tokens[0].Equals("BR")) // BR 부분 (브로드캐스트)
+            {
+                fromID = tokens[1].Trim();
+                string msg = tokens[2];
+                Console.WriteLine("[전체]{0}:{1}", fromID, msg);
+                //
+
+                s.Send(Encoding.Unicode.GetBytes("BR_Success:"));
+            }
+            else if (code.Equals("TO"))
+            {
+                fromID = tokens[1].Trim();
+                toID = tokens[2].Trim();
+                string msg = tokens[3];
+                string rMsg = "[From:" + fromID + "][TO:" + toID + "]" + msg;
+                Console.WriteLine(rMsg);
+
+                //
+                SendTo(toID, m);
+                s.Send(Encoding.Unicode.GetBytes("To_Success:"));
+                
+            }
+            else
+            {
+                Broadcast(s, m);
+            }
+        }
+
+        void SendTo(string id, string m)
+        {
+            Socket socket;
+            byte[] bytes = Encoding.Unicode.GetBytes(msg);
+            if (connectedClients.ContainsKey(id))
+            {
+                // 
+                connectedClients.TryGetValue(id, out socket!);
+                try { 
+                    socket.Send(bytes); 
+                } catch {
+                    Disconnected(client.Value);
+                }
+            }
+        }
+
+        void Broadcast(Socket s, string msg) // 5-2ㅡ모든 클라이언트에게 Send, 보낸사람 s (Socket 값), 보낸메세지 msg (메세지 값) 
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(msg);
+            //
+            {
+                foreach (keyvaluepair<string, Socket> client in connectedClients.ToArray())
+                {
+                    try
+                    {
+                        //5-2 send
+                        //
+                        if (s! = clinet.Value)
+                        {
+                            client.Value.Send(bytes);
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        Disconnected(client.Value);
+                    }
+                }
+            }
+        }
+    }
+}
