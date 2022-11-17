38b037738f0a87b43b0eebb3dfe6315a30f877e8
toast-ceo <70907737+toast-ceo@users.noreply.github.com>
2022-11-17 17:07:27 +0900

    2022.11.17

---
+++ network/Client/Program.cs
@@ -0,0 +1,153 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+
+namespace AClient
+{
+    public class Client
+    {
+        private readonly static int BufferSize = 4096;
+
+        public static void Main()
+        {
+            try
+            {
+                new Client().Init();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("Press any key to exit the program.");
+            Console.ReadKey();
+        }
+
+
+        private Socket clientSocket;
+        public Socket ClientSocket
+        {
+            get => clientSocket;
+            set => clientSocket = value;
+        }
+        private readonly IPEndPoint EndPoint = new(IPAddress.Parse("127.0.0.1"), 5001);
+
+        public Client()
+        {
+            ClientSocket = new(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp
+            );
+        }
+
+        void Init()
+        {
+            ClientSocket.Connect(EndPoint);
+            Console.WriteLine($"Server connected.");
+
+            SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+            args.Completed += new EventHandler<SocketAsyncEventArgs>(Received);
+            ClientSocket.ReceiveAsync(args);
+
+            Send();
+        }
+
+
+        void Received(object? sender, SocketAsyncEventArgs e)
+        {
+            try
+            {
+                byte[] data = new byte[BufferSize];
+                Socket server = (Socket)sender!;
+                int n = server.Receive(data);
+
+                string str = Encoding.Unicode.GetString(data);
+                str = str.Replace("\0", "");
+                Console.WriteLine("수신:" + str);
+
+
+                SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+                args.Completed += new EventHandler<SocketAsyncEventArgs>(Received);
+                ClientSocket.ReceiveAsync(args);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"Server disconnected.");
+                ClientSocket.Close();
+            }
+        }
+
+        void Send()
+        {
+            byte[] dataID;
+            Console.WriteLine("ID를 입력하세요");
+            string nameID = Console.ReadLine()!;
+            //
+            string message = "ID:" + nameID + ":";
+            dataID = Encoding.Unicode.GetBytes(message);
+            clientSocket.Send(dataID);
+            //
+
+            Console.WriteLine("특정 사용자에게 보낼 때는 사용자ID:메시지 로 입력하시고\n" +
+                "브로드캐스트하려면 BR:메시지 를 입력하세요");
+            do
+            {
+                byte[] data;
+                string msg = Console.ReadLine()!;
+                string[] tokens = msg.Split(':');
+                string m;
+                if (tokens[0].Equals("BR"))
+                {
+                    //
+                    m = "BR:" + nameID + ":" + tokens[1] + ":";
+
+                    data = Encoding.Unicode.GetBytes(m);
+                    Console.WriteLine("[전체전송]{0}", tokens[1]);
+                    try { ClientSocket.Send(data); } catch { }
+                }
+                else if (tokens[0].Equals("File"))
+                {
+                    SendFile(tokens[1]);
+                }
+                else //  (tokens[0].Equals("TO"))
+                {
+                    //
+                    m = "TO:" + nameID + ":" + tokens[0] + ":" + tokens[1] + ":";
+                    data = Encoding.Unicode.GetBytes(m);
+                    Console.WriteLine("[{0}에게 전송]:{1}", tokens[0], tokens[1]);
+                    try { ClientSocket.Send(data); } catch { }
+                }
+
+                
+
+
+            } while (true);
+        }
+        void SendFile(string filename)
+        {
+            FileInfo fi = new FileInfo(filename);   
+            string fileLength = fi.Length.ToString();
+
+            byte[] bDts = Encoding.Unicode.GetBytes
+                ("File:" + filename + ":" + fileLength + ":");
+            clientSocket.Send(bDts);
+
+            byte[] bDtsRx = new byte[4096];
+            // FileShare - 파일 공유 나혼자 
+            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.None);
+            long received = 0;
+            while (received < fi.Length)
+            {
+                received += fs.Read(bDtsRx, 0, 4096);
+                clientSocket.Send(bDtsRx);
+                Array.Clear(bDtsRx); 
+            }
+            fs.Close();
+            Console.WriteLine("파일 송신 종료");
+        }
+
+    }
+}
---
+++ network/Server/Program.cs
@@ -0,0 +1,220 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace AServer
+{
+    public class Server
+    {
+        private readonly static int BufferSize = 4096;
+
+        public static void Main()
+        {
+            try
+            {
+                new Server().Init();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+
+        private Dictionary<string, Socket> connectedClients = new();
+
+        public Dictionary<string, Socket> ConnectedClients
+        {
+            get => connectedClients;
+            set => connectedClients = value;
+        }
+
+        private Socket ServerSocket;
+
+        private readonly IPEndPoint EndPoint = new(IPAddress.Parse("127.0.0.1"), 5001);
+
+        int clientNum;
+        Server()
+        {
+            ServerSocket = new(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp
+            );
+            clientNum = 0;
+        }
+
+        void Init()
+        {
+            ServerSocket.Bind(EndPoint);
+            ServerSocket.Listen(100);
+            Console.WriteLine("Waiting connection request.");
+
+            Accept();
+
+        }
+
+
+        void Accept()
+        {
+            do
+            {
+                Socket client = ServerSocket.Accept();
+
+
+                Console.WriteLine($"Client accepted: {client.RemoteEndPoint}.");
+
+                SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+                args.Completed += new EventHandler<SocketAsyncEventArgs>(Received);
+                client.ReceiveAsync(args);
+
+            } while (true);
+        }
+
+        void Disconnected(Socket client)
+        {
+            Console.WriteLine($"Client disconnected: {client.RemoteEndPoint}.");
+            foreach (KeyValuePair<string, Socket> clients in connectedClients)
+            {
+                if (clients.Value == client)
+                {
+                    ConnectedClients.Remove(clients.Key);
+                    clientNum--;
+                }
+            }
+            client.Disconnect(false);
+            client.Close();
+        }
+
+        void Received(object? sender, SocketAsyncEventArgs e)
+        {
+            Socket client = (Socket)sender!;
+            byte[] data = new byte[BufferSize];
+            try
+            {
+                int n = client.Receive(data);
+                if (n > 0)
+                {
+
+                    //
+                    MessageProc(client, data);
+
+                    SocketAsyncEventArgs argsR = new SocketAsyncEventArgs();
+                    argsR.Completed += new EventHandler<SocketAsyncEventArgs>(Received);
+                    client.ReceiveAsync(argsR);
+                }
+                else { throw new Exception(); }
+            }
+            catch (Exception)
+            {
+                Disconnected(client);
+            }
+        }
+
+        void MessageProc(Socket s, byte[] bytes)
+        {
+            string m = Encoding.Unicode.GetString(bytes);
+            //
+            string[] tokens = m.Split(':');
+            string fromID;
+            string toID;
+            string code = tokens[0];
+
+            if (code.Equals("ID"))
+            {
+                clientNum++;
+                fromID = tokens[1].Trim();
+                Console.WriteLine("[접속{0}]ID:{1},{2}",
+                    clientNum, fromID, s.RemoteEndPoint);
+                //
+                connectedClients.Add(fromID, s);
+                s.Send(Encoding.Unicode.GetBytes("ID_REG_Success:"));
+                Broadcast(s, m);
+            }
+            else if (tokens[0].Equals("BR"))
+            {
+                fromID = tokens[1].Trim();
+                string msg = tokens[2];
+                Console.WriteLine("[전체]{0}:{1}", fromID, msg);
+                //
+                Broadcast(s, m);
+                s.Send(Encoding.Unicode.GetBytes("BR_Success:"));
+            }
+            else if (code.Equals("TO"))
+            {
+                fromID = tokens[1].Trim();
+                toID = tokens[2].Trim();
+                string msg = tokens[3];
+                string rMsg = "[From:" + fromID + "][TO:" + toID + "]" + msg;
+                Console.WriteLine(rMsg);
+
+                //
+                SendTo(toID, m);
+                s.Send(Encoding.Unicode.GetBytes("To_Success:"));
+            }
+            else if (code.Equals("File"))
+            {
+                ReceiveFile(s, m);
+            }
+            else
+            {
+                Broadcast(s, m);
+            }
+        }
+        void ReceiveFile(Socket s, string m)
+        {
+            string output_path = "FileDown";
+            if (!Directory.Exists(output_path))
+            {
+                Directory.CreateDirectory(output_path); 
+            }
+            string[] tokens = m.Split(':');
+            string fileName = tokens[1].Trim();
+            long fileLength = Convert.ToInt64(tokens[2].Trim());
+            string FileDest = output_path +fileName;
+
+            long flen = 0;
+            FileStream fs = new FileStream(FileDest, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None);
+            while(flen < fileLength)
+            {
+                byte[] fdata = new byte[4096];
+                int r = s.Receive(fdata, 0, 4096, SocketFlags.None);
+                fs.Write(fdata, 0, r);
+                flen += r;
+            }
+            fs.Close(); 
+        }
+        void SendTo(string id, string msg)
+        {
+            Socket socket;
+            byte[] bytes = Encoding.Unicode.GetBytes(msg);
+            if (connectedClients.ContainsKey(id))
+            {
+                //
+                connectedClients.TryGetValue(id, out socket!);
+                try { socket.Send(bytes); } catch { }
+            }
+        }
+        void Broadcast(Socket s, string msg) // 5-2ㅡ모든 클라이언트에게 Send
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(msg);
+            //
+            foreach (KeyValuePair<string, Socket> client in connectedClients.ToArray())
+            {
+                try
+                {
+                    //5-2 send
+                    //
+                    if (s != client.Value)
+                        client.Value.Send(bytes);
+
+                }
+                catch (Exception)
+                {
+                    Disconnected(client.Value);
+                }
+            }
+        }
+
+    }
+}
